ef43ca1936da05e8df3767137f39ced448d5a681
kaojunnn <863646260@qq.com>
2023-05-07 14:08:53 +0800

    23.5.7
    做了角色的移动，收拾了一下文件夹

---
+++ BooomGame/Assets/Kao_jun/Script/GameInputManage.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInputManage : MonoBehaviour
+{
+    public float speed = 5f;
+    private Vector3 moveInput;
+    private Rigidbody playerRig;
+
+    private void Start()
+    {
+        if (transform.TryGetComponent<Rigidbody>(out playerRig))
+        {
+            Debug.Log("Found Rigidbody component: " + playerRig.name);
+        }
+        else
+        {
+            Debug.Log("Could not find Rigidbody component");
+        }
+    }
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+        
+        position.x += moveInput.x * speed * Time.deltaTime;
+        position.z += moveInput.y * speed * Time.deltaTime;//暂时是在zx平面上移动
+
+        playerRig.MovePosition(position);
+        
+  
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+ 
+}
