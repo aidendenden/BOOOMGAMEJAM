3eb7fe822e6ee1bf5b02ab9225ffa12c90e86a07
kaojunnn <863646260@qq.com>
2023-05-08 23:38:27 +0800

    23.5.8.23：36
    简单地搭建了场景，给player做了简单的动画控制，我都放在kao_jun文件夹里面了，场景也是，关于那个墙的碰撞有点问题，好像能从背面穿过去……不过感觉可以等到细致搭建的时候再精修一下

--- BooomGame/Assets/Kao_jun/Script_K/GameInputManage_K.cs
+++ BooomGame/Assets/Kao_jun/Script_K/GameInputManage_K.cs
@@ -9,6 +9,8 @@ public class GameInputManage_K : MonoBehaviour
     public float speed = 5f;
     private Vector3 moveInput;
     private Rigidbody playerRig;
+    private Animator _animator;
+    private float StopX_k, StopY_k;//用于储存输入的方向传给animator中判断方向
 
     private void Start()
     {
@@ -20,6 +22,17 @@ public class GameInputManage_K : MonoBehaviour
         {
             Debug.Log("Could not find Rigidbody component");
         }
+
+
+        if(GameObject.FindGameObjectWithTag("Player").TryGetComponent<Animator>(out _animator))
+        {
+            Debug.Log("Found Animator component: " + _animator.name);
+        }
+        else
+        {
+            Debug.Log("Could not find Animator component");
+        }
+
     }
 
     void Update()
@@ -29,8 +42,9 @@ public class GameInputManage_K : MonoBehaviour
         position.x += moveInput.x * speed * Time.deltaTime;
         position.z += moveInput.y * speed * Time.deltaTime;//暂时是在zx平面上移动
 
-        playerRig.MovePosition(position);
+        WalkAnimationController_K();//判断player动画
 
+        playerRig.MovePosition(position);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -38,4 +52,29 @@ public class GameInputManage_K : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
  
+
+
+
+
+
+
+
+    private void WalkAnimationController_K()//这个是给主角判断动画的
+    {
+        if (moveInput != Vector3.zero)
+        {
+            _animator.SetBool("isRuning", true);
+            StopX_k = moveInput.x;
+            StopY_k = moveInput.y;//如果在移动就更新用于判断动画状态的xy
+
+        }
+        else
+        {
+            _animator.SetBool("isRuning", false);//不在移动就不更新用于动画判断的xy
+        }
+
+        _animator.SetFloat("inputX", StopX_k);
+        _animator.SetFloat("inputY", StopY_k);
+
+    }
 }
