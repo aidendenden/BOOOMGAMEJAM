60d3d00475783809c4964ddc60e96ac149d6188c
aiden233 <2996876880@qq.com>
2023-05-14 20:39:13 +0800

    update
    改存档系统和打包设置，以及其他七七八八的

--- BooomGame/Assets/Script/Manager/PlayerManager.cs
+++ BooomGame/Assets/Script/Manager/PlayerManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 /// <summary>
 /// 在这个脚本里的Triggered方法决定传什么
 /// </summary>
+[AddComponentMenu("GameInputManage")]
 public class PlayerManager : MonoBehaviour
 {
     private static readonly Lazy<PlayerManager> Lazy = new Lazy<PlayerManager>(() => new PlayerManager());
@@ -34,3 +35,21 @@ public class PlayerManager : MonoBehaviour
     }
     
 }
+
+#region 监听交互的方法
+// 以下是监听交互的方法
+// void OnEnable()
+// {
+//     PlayerManager.OnTrigger += HandleTrigger;
+// }
+//
+// void OnDisable()
+// {
+//     PlayerManager.OnTrigger -= HandleTrigger;
+// }
+//
+// void HandleTrigger(string message,EGameObj gameObj)
+// {
+//     Debug.Log("Trigger event received:222222222222 " + message,StuffEnum);
+// }
+#endregion
